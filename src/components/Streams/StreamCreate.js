4734c5452466ae8c46d13dc11e7efa488fc44d5f
Linda Wang <lindawang256@gmail.com>
2019-04-02 16:32:17 -0700

    Add semantic ui error message

--- src/components/Streams/StreamCreate.js
+++ src/components/Streams/StreamCreate.js
@@ -2,22 +2,45 @@ import React from 'react';
 import {Field, reduxForm} from 'redux-form'
 
 class StreamCreate extends React.Component{
-    renderInput(formProps){
-        console.log('formProps',formProps)
-        let errorStyle={};
-        if(formProps.meta.touched===false){
-            errorStyle={display:'none'}
+    // renderInput(formProps){
+    //     console.log('formProps',formProps)
+    //     let errorStyle={};
+    //     if(formProps.meta.touched===false){
+    //         errorStyle={display:'none'}
+    //     }
+    //     return(
+    //         <div className="field">
+    //             <label>{formProps.label}</label>
+    //             <input {...formProps.input}/>
+    //             <div style={errorStyle}>{formProps.meta.error}</div>
+    //         </div>
+            
+    //     )
+
+    // }
+    renderError=({error,touched})=>{
+        if(touched && error){
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            )
         }
+    }
+    renderInput=(formProps)=>{
+        console.log('formProps',formProps)
+        const className=`field ${formProps.meta.error && formProps.meta.touched ? 'error':''}`
         return(
-            <div className="field">
+            <div className={className}>
                 <label>{formProps.label}</label>
                 <input {...formProps.input}/>
-                <div style={errorStyle}>{formProps.meta.error}</div>
+                {this.renderError(formProps.meta)}
             </div>
             
         )
 
     }
+
     onSubmit(formValues){
         console.log('form value', formValues)
 
@@ -25,7 +48,9 @@ class StreamCreate extends React.Component{
     render(){
         //console.log("props in StreamCreate",this.props)
         return(
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+            <form 
+                onSubmit={this.props.handleSubmit(this.onSubmit)} 
+                className="ui form error">
                 <Field name="title" component={this.renderInput} label="Enter Title"/>
                 <Field name="description"  component={this.renderInput} label="Enter Description"/>
                 <button>Submit</button>
